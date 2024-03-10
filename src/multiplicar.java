f34910662d31930799e6851535c35b3b3812135e
Pedro Cruz <160654169+Pedrocrg@users.noreply.github.com>
2024-03-10 14:44:51 +0100

    Merge pull request #1
    * Menu, solicitud de datos y switch que los represente por consola
    
    * Clase para la suma con sus métodos correspondientes.
    
    * Clase para la resta con sus métodos correspondientes.
    
    * Clase para la multiplicacion con sus métodos correspondientes.
    
    * Clase para la división con sus métodos correspondientes.

---
+++ src/multiplicar.java
@@ -0,0 +1,15 @@
+public class multiplicar {
+
+    private double num1;
+    private double num2;
+
+    public multiplicar(double num1, double num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+
+    public static double multiplicacion(double num1, double num2){
+        double result=num1*num2;
+        return result;
+    }
+}
---
+++ src/restar.java
@@ -0,0 +1,15 @@
+public class restar {
+
+    private double num1;
+    private double num2;
+
+    public restar(double num1, double num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+
+    public static double resta(double num1, double num2){
+        double result=num1-num2;
+        return result;
+    }
+}
