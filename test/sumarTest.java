4a5e1732a51e9337db07171ae87e4182b1ce6abd
Pedrocrg <160654169+Pedrocrg@users.noreply.github.com>
2024-03-10 15:09:12 +0100

    Batería de 3 pruebas que verifacan funcionamiento comlpetada.

--- test/sumarTest.java
+++ test/sumarTest.java
@@ -14,21 +14,22 @@ class sumarTest {
 
     @org.junit.jupiter.api.Test
     void suma() {
-        double[] valores = {5,5,5};
-        double expResult = 5.0;
-        double result = sumar.suma(valores);
+        double val1=5;
+        double val2=5;
+        double expResult = 10.0;
+        double result = sumar.suma(val1,val2);
         assertEquals(expResult, result);
 
-        valores[0]=7;
-        valores[1]=9;
-        valores[2]=8;
-        expResult = 8.0;
-        result = Main.calcularMedia(valores);
+        val1=7;
+        val2=9;
+        expResult = 16.0;
+        result = sumar.suma(val1, val2);
         assertEquals(expResult, result);
 
-        valores = {5,5,5};
-        expResult = 5.0;
-        result = Main.calcularMedia(valores);
+        val1=10;
+        val2=3;
+        expResult = 13.0;
+        result = sumar.suma(val1, val2);
         assertEquals(expResult, result);
     }
 }
